45a0564afeffe80f1924be012295fc7fc9742e28
JacksonGoulden <127802658+JacksonGoulden@users.noreply.github.com>
2023-03-19 13:05:22 +1300

    Added support for siri voice

--- test.py
+++ test.py
@@ -7,12 +7,16 @@ def lambda_handler(event, context):
   openai.organization = "org-uDYGnAtAj6JZSVli5KOYHmLa"
   openai.api_key = "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"
     
+  text_to_ask = event["headers"]["gpt-phrase"]
+  
   completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "user", "content": "Tell me a single cat fact"}
+    {"role": "user", "content": text_to_ask}
     ]
   )
+  
+  print(json.dumps(event))
   
   notification = str(completion.choices[0].message.content)
   
@@ -22,6 +26,8 @@ def lambda_handler(event, context):
   #   Message = json.dumps({'default': notification}),
   #   MessageStructure = 'json'
   #   )
+  
+  print(event["headers"]["gpt-phrase"])
   
   return {
     'statusCode': 200,

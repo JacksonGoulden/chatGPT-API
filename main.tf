7ec2e1f8d777a070f2bfba8349133cfa977ec70a
elphep <jackson.goulden@gmail.com>
2023-04-16 13:36:00 +1200

    changed from cloudformation to TF

--- main.tf
+++ main.tf
@@ -1,5 +1,45 @@
-resource "aws_cloudformation_stack" "openai-lambda-layer" {
-  name = "openai-layer"
-  template_body = file("custom_lambda_layer.yaml")
-  capabilities = ["CAPABILITY_NAMED_IAM"]
+
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = "layer.zip"
+  layer_name = "openai-layer"
+
+  compatible_runtimes = ["python3.9"]
+}
+
+data "archive_file" "lambda-app-zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_app.py"
+  output_path = "${path.module}/lambda_app.zip"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "openai-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = data.archive_file.lambda-app-zip.output_path
+  function_name = "Siri-chatGPT"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+
+  layers = [aws_lambda_layer_version.lambda_layer.arn]
+  
+  runtime = "python3.9"
+
 }
